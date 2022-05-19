4c2bdce2e1488c0d5d86e0615ebb91800946a880
Abed Khalaf <kfabdall@gmail.com>
2022-05-19 15:35:34 +0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                HELLO WORLD!!!
+            </div>
+        )
+    }
+}
+
+const root = ReactDOM.createRoot(
+   document.getElementById('root')
+  );
+  root.render(<App />);
