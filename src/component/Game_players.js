48bae789e4f56b48ea6726886f6d90d10b0f7d35
Abed Khalaf <kfabdall@gmail.com>
2022-05-20 17:58:57 +0300

    css updated

--- src/component/Game_players.js
+++ src/component/Game_players.js
@@ -1,22 +1,23 @@
 import './Game_players.css'
 
 import React from 'react';
+
 class Players extends React.Component {
    render() {
        return (
            <div>
              <div className='Player1'>
-                  <h1>Player 1</h1>
-                  <label>0</label>
-                  <div className='CURRENT_SCORE'>
+                  <h1 className='players-title'>Player 1</h1>
+                  <label className='score'>0</label>
+                  <div className='current_score'>
                       <label>Current</label>
                       <label> 0</label>
                   </div>
              </div>
              <div className='Player2'>
-                  <h1>Player 2</h1>
-                  <label>0</label>
-                  <div className='CURRENT_SCORE'>
+                  <h1 className='players-title'>Player 2</h1>
+                  <label className='score'>0</label>
+                  <div className='current_score'>
                       <label>Current </label>
                       <label>0</label>
                   </div>
