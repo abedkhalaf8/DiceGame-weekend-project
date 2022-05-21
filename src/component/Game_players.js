ebff6970852822cee4fe597c30ec7c3937999326
Abed Khalaf <kfabdall@gmail.com>
2022-05-21 13:58:01 +0300

    function update

--- src/component/Game_players.js
+++ src/component/Game_players.js
@@ -3,23 +3,23 @@ import './Game_players.css'
 import React from 'react';
 
 class Players extends React.Component {
-   render() {
+render() {
        return (
            <div>
              <div className='Player1'>
                   <h1 className='players-title'>Player 1</h1>
-                  <label className='score'>{this.props.score}</label>
+                  <label className='score'>0</label>
                   <div className='current_score'>
                       <label id='current'>CUTRRENT </label>
-                      <label>{this.props.current_score}</label>
+                      <label>0</label>
                   </div>
              </div>
              <div className='Player2'>
                   <h1 className='players-title'>Player 2</h1>
-                  <label className='score'>{this.props.score}</label>
+                  <label className='score'>0</label>
                   <div className='current_score'>
                       <label id='current'>CUTRRENT </label>
-                      <label>{this.props.current_score}</label>
+                      <label>0</label>
                   </div>
              </div>
            </div>
