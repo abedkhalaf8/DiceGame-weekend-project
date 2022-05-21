678c4c710fe297cf86cf6f3c7f958baabcbddaac
Abed Khalaf <kfabdall@gmail.com>
2022-05-21 22:27:03 +0300

    update game component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import App from './component/App';
+import GameBoard from './component/game_board';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+class App extends React.Component {
+    render() {
+        return (
+             <div>
+                 <GameBoard />
+                 <p>MADE BY ABED KHALAF</p>
+             </div>
+         )
+     }
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
